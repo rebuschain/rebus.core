2bd70031d6c380d7d2cf4de8cdf546b060f54260
pier <pierstabilini@gmail.com>
2022-09-03 15:58:30 +0100

    add upgrade info

--- app/upgrades/v1/constants.go
+++ app/upgrades/v1/constants.go
@@ -6,5 +6,5 @@ const (
 	// MainnetUpgradeHeight defines the Rebus mainnet block height on which the upgrade will take place
 	TestNetUpgradeHeight = (24*60*60)/3 + 1
 	// UpgradeInfo defines the binaries that will be used for the upgrade
-	UpgradeInfo = ``
+	UpgradeInfo = `{"binaries": {"linux/x86_64": "https://github.com/rebuschain/rebus.core/releases/download/v0.0.4/rebus_0.0.4_Linux_x86_64.tar.gz"}}`
 )
